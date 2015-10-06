72bb91fa52f7c06af43755a2f6c482a74ce90699
Antony Vincent <avincent@Carbide-2014.05>
2015-10-06 18:49:01 -0400

    RAxML_driver.sh: We are now able to perform rapid bootstraps. -launchpad: the output files are no longer removed for the moment. -pangenome4_fast.pl: each usearch process now uses 1 core instead of the maximum.

--- bin/RAxML_driver.sh
+++ bin/RAxML_driver.sh
@@ -3,15 +3,10 @@
 
 awk 'BEGIN{FS=OFS="\t"}{$1="";sub("\t","")}1' binary_matrix.tsv > binary_formated.tsv
 
-Rscript /home/avincent/Desktop/script_luca_propre/bin/transpose.r
+Rscript /home/avincent/Desktop/saturnV/bin/transpose.r
 sed -i -e "1d" matrix_transposed.tsv
 
 sed 's/ //g;s/^./>/;s/"/\n/g' matrix_transposed.tsv > binary_matrix.fasta
 
-raxmlHPC-PTHREADS-AVX -m BINGAMMA -s binary_matrix.fasta -p 12345 -n phylogeny -T $1
-
-raxmlHPC-PTHREADS-AVX -m BINGAMMA -s binary_matrix.fasta -p 12345 -b 12345 -# $2 -n bootstrap -T $1
-
-raxmlHPC-PTHREADS-AVX -f b -m BINGAMMA -t RAxML_bestTree.phylogeny -z RAxML_bootstrap.bootstrap -n tree -T $1
-
+raxmlHPC-PTHREADS-AVX -f a -p 12345 -s binary_matrix.fasta -x 12345 -# "$2" -m BINGAMMA -T "$1" -n phylogeny
 
--- bin/pangenome4_fast.pl
+++ bin/pangenome4_fast.pl
@@ -46,7 +46,7 @@ for $genome (@genomes[0..$#genomes]){
   
   $manager->start and next;
     print "::performing usearch searches -- ${first_genome} vs ${genome}\n";
-    `usearch8 -usearch_local $first_genome -db ${genome}.udb -id $identity_usearch -blast6out ${genome}_blasted.txt >> log_file 2>&1`;
+    `usearch8 -usearch_local $first_genome -threads 1 -db ${genome}.udb -id $identity_usearch -blast6out ${genome}_blasted.txt >> log_file 2>&1`;
 
   $manager->finish;
 
@@ -276,7 +276,7 @@ for $genome (@genomes){
   $manager->start and next;
     print "::performing usearch searches -- new_sequences_iter1 vs ${genome}\n";
 
-    `usearch8 -usearch_local new_sequences_iter1.faa -db ${genome}.udb -id $identity_usearch -blast6out ${genome}_blasted_iter2.txt >> log_file 2>&1`;
+    `usearch8 -usearch_local new_sequences_iter1.faa -threads 1 -db ${genome}.udb -id $identity_usearch -blast6out ${genome}_blasted_iter2.txt >> log_file 2>&1`;
 
   $manager->finish;
     
