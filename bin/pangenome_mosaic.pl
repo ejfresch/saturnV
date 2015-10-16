ec488ef71faf09bead461cebab90cff087eef32c
Luca Freschi <luca.freschi@bio.ulaval.ca>
2015-10-16 14:51:40 -0400

    --pangenome_mosaic.pl: uses mummer to compare finished or draft genomes to a reference genome --reconstruct_mosaic.pl: reads the output of pangenome_mosaic.pl and returns the coverage and a binary fasta file

--- bin/pangenome_mosaic.pl
+++ bin/pangenome_mosaic.pl
@@ -1,10 +1,20 @@
-#!/usr/bin/perl -I /home/lfreschi/tasks/pangenome/script_new/bin
+#!/usr/bin/perl
 
 use Parallel::ForkManager;
 use Getopt::Long;
 
+$n_cpu=1;
+$dir_genomes="";
+$out_dir="";
 
 GetOptions ("d=s" => \$dir_genomes,"r=s" => \$ref,"out=s" => \$out_dir,"c=s"   => \$n_cpu) or die("::usage: $0 -d <dir_genomes> -r <ref_genome> -out <out_dir> -c <n_cpu>\n");
+
+if(($dir_genomes eq "") or ($out_dir eq "")){
+    print "::usage: $0 -d <dir_genomes> -r <ref_genome> -out <out_dir> -c <n_cpu>\n";
+    exit();
+
+}
+
 
 mkdir($out_dir);
 
@@ -18,23 +28,40 @@ print "::Starting analysis at $date";
 chomp($dir_genomes);
 print "::DIR_GENOMES=".$dir_genomes."\n";
 
+
+if((!(-e "${dir_genomes}/$ref")) and (!(-e $ref))){
+    print "::Sorry! I cannot find $ref\n";
+    exit();
+}
+
+
 @genomes=`ls -1 $dir_genomes`;
 chomp(@genomes);
 
-
+$ref_abs=$ref;
+$ref=`basename $ref_abs`;
+chomp($ref);
 $root_ref=$ref;
 $root_ref=~s/.fasta//;
 $root_ref=~s/_/-/g;
 
-print "::trimming the name of scaffolds -- $ref\n";
-`trim_name_scaffolds.pl ${dir_genomes}/$ref $ref $root_ref`;
+if(-e "${dir_genomes}/$ref"){
+    print "::trimming the name of scaffolds -- $ref\n";
+    `trim_name_scaffolds.pl ${dir_genomes}/$ref ${out_dir}/$ref $root_ref`;
+    
+}else{
+    print "::trimming the name of scaffolds -- $ref\n";
+    `trim_name_scaffolds.pl ${ref_abs} ${out_dir}/$ref $root_ref`;
+
+
+}
 
 
 
 foreach $genome (@genomes){
 
     if($genome eq $ref){next;}
-
+    #print $genome."\n";
     $root=$genome;
     $root=~s/.fasta//;
     $root=~s/_/-/g;
@@ -54,12 +81,12 @@ foreach $genome (@genomes){
     
     
     print "::trimming the name of scaffolds -- $genome\n";
-    `trim_name_scaffolds.pl ${dir_genomes}/$genome ${genome} $root`;
+    `trim_name_scaffolds.pl ${dir_genomes}/$genome ${out_dir}/${genome} $root`;
     
     print "::launching mummer -- $ref vs $genome\n";
     
     
-    $cmd="mummer -b -c -L $ref $genome 1>${out_dir}/${root_ref}_vs_${root}.mum 2>>logs_mummer.txt";
+    $cmd="mummer -b -c -L ${out_dir}/$ref ${out_dir}/$genome 1>${out_dir}/${root_ref}_vs_${root}.mum 2>>${out_dir}/logs_mummer.txt";
     system($cmd);
 
   $manager->finish;
