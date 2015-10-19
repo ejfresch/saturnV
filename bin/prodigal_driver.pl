fc567002d27145d005dd8f0a555c14dd1bfc2449
Antony Vincent <avincent@Carbide-2014.05>
2015-10-19 14:19:53 -0400

    -prodigal_driver.pl: Fixed the problem related to the underscores. Also transpose.r is no longer dependent of the path.

--- bin/prodigal_driver.pl
+++ bin/prodigal_driver.pl
@@ -38,11 +38,32 @@ foreach $genome (@genomes){
     print "::trimming the name of scaffolds -- $genome\n";
     `trim_name_scaffolds.pl ${dir_genomes}/$genome ${genome} $root`;
     
-    print "::launching prodigal for genome $genome\n";
-    
-    
+    print "::launching prodigal for genome $genome\n";    
     `prodigal -i $genome -a ${root}/${root}.faa -f gff -o ${root}/${root}.gff >> log_file 2>&1`;
     
+
+
+
+	open($faa_clean, '>', "${root}/${root}_clean.faa");
+	open(IN1,"${root}/${root}.faa")||die "I cannot open ${root}/${root}.faa";
+	$i=1;
+   	 while($line1=<IN1>){
+     	   chomp($line1);
+
+		if ($line1 =~ /^\>/)
+				{
+				print $faa_clean ">${root}_seq$i\n";
+				$i++;
+				}
+		else
+		{
+		print $faa_clean "$line1\n";
+		}
+
+	}
+	close(IN1);
+	close("$faa_clean");
+	system("mv ${root}/${root}_clean.faa ${root}/${root}.faa");
 
   $manager->finish;
 }
