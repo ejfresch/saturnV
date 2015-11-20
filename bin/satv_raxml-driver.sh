d67ba02c65c683ce0cc500c168422ecb6684cd92
Antony Vincent <antony.vincent.1@ulaval.ca>
2015-11-20 17:26:37 -0500

    satv_raxml-driver.sh: changed the binary for a standard PTHREADS version without the AVX optimization (should normally works with all processor types)

--- bin/satv_raxml-driver.sh
+++ bin/satv_raxml-driver.sh
@@ -8,5 +8,5 @@ sed -i -e "1d" matrix_transposed.tsv
 
 sed 's/ //g;s/^./>/;s/"/\n/g' matrix_transposed.tsv > binary_matrix.fasta
 
-raxmlHPC-PTHREADS-AVX -f a -p 12345 -s binary_matrix.fasta -x 12345 -# "$2" -m BINGAMMA -T "$1" -n phylogeny
+raxmlHPC-PTHREADS -f a -p 12345 -s binary_matrix.fasta -x 12345 -# "$2" -m BINGAMMA -T "$1" -n phylogeny
 
