b006ad3181be46ec0f543a460a208e479cc01569
Luca Freschi <luca.freschi@bio.ulaval.ca>
2016-05-02 11:54:00 -0400

    --README: added some contents --satv_clean-directory.pl: minor (but important) changes.

--- bin/satv_clean-directory.pl
+++ bin/satv_clean-directory.pl
@@ -1,5 +1,14 @@
 #!/usr/bin/perl
 
+use Getopt::Long;
+
+
+$method="";
+
+GetOptions("m=s" => \$method,) or die("::usage: $0 -m <method>\n");
+
+
+
 print "::removing Phylogeny\n";
 `rm -rf Phylogeny`;
 print "::removing prot_files\n";
@@ -21,3 +30,20 @@ print "::removing genomes_to_analyze.txt\n";
 `rm -rf genomes_to_analyze.txt`;
 
 
+if($method eq "strict"){
+ print "::removing fasta_subject files\n";
+`rm -rf fasta_subject*`;   
+ print "::removing screen_paralogs files\n";
+`rm -rf screen_paralogs.txt`;   
+ print "::removing commands-* files\n";
+`rm -rf commands-*`;
+ print "::removing usearch-resolve-line-* files\n";
+`rm -rf usearch_resolve_line-*`;
+ print "::removing pre-table_linked5_strict.tsv\n";
+`rm -rf pre-table_linked5_strict.tsv`;   
+ print "::removing table_linked5_strict.tsv\n";
+`rm -rf table_linked5_strict.tsv`;   
+
+  
+
+}
